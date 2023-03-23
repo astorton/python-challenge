0d35561933bfa46ca1e14b64aab5bff9629e5b45
Alex <astorton760@gmail.com>
2023-03-23 14:10:50 -0700

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,49 +1,78 @@
+#step 0: import the csv
 import csv
 
 #step 1: identify the csv path
-
 csvfilepath = "Resources/budget_data.csv"
 
-#Establish the known lists for use in iteration runs
-total_pl = []
-total_months = []
-monthly_profit_delta = []
+#Set the variables
+month_count = 0
+total_pl = 0
+monthly_profit_delta = 0
+previous_pl_value = 0
+monthly_delta_value = 0
+rolling_delta_value = 0
+rolling_change_list = []
+rolling_delta_value_count = 0
+max_value = 0
+min_value = 0
+max_value_month ="max value month"
+min_value_month = "min value month"
 
-#Open the file and let's get started! 
+#step 3: open the file and let's get started! 
 with open(csvfilepath,mode='r',encoding='UTF-8') as csv_file:
     csv_data = csv.reader(csv_file,delimiter=',')
 
-    #Step 0: Skip the headers for all iteration runs!
+    #Step 4: Skip the headers for all iteration runs!
     headers = next(csv_data)
     
-    #Step 1: print count of total, non-header records
-    #months_count = str((len(list(csv_data))))
+    #Step 5: Pull csv_data values and push to lists that will be used for iteration runs
 
-    #Step 2: Pull csv_data values and push to lists that will be used for iteration runs
+    for i in csv_data:
+        month_count = month_count + 1
+        total_pl = total_pl + int(i[1])
+        #produce a rolling monthly change value as an output of each iteration
+        rolling_delta_value = int(i[1]) - previous_pl_value
+        #identify the highest value by comparing the latest claimant of the title. 
+        if rolling_delta_value > max_value:
+            max_value = rolling_delta_value
+            max_value_month = str(i[0])
+        #identify the lowest value by comparing the latest claimant of the title. 
+        if rolling_delta_value < min_value:
+            min_value = rolling_delta_value
+            min_value_month = str(i[0])
+        #as we iterate, add to the rolling change list with all monthly changes produced at the beginning of the for loop
+        rolling_change_list.append(int(rolling_delta_value))
+        #update the previous_profit/loss value with the current iteration value so the next run can evaluate the next row against the current to identify the next change
+        previous_pl_value = int(i[1])
 
-    for record in csv_data:
-        total_months.append(record[0])
-         #This help will calculate net total P/L over ENTIRE period
-        total_pl.append(int(record[1]))
-          
-    #Step 3: Gather monthly change in profit(s)
-    for record in range(len(total_pl)-1):
-        monthly_profit_delta.append(total_pl[record+1]-total_pl[record])
+    #prep for the average change of all change values by removing 1 from total month count to get the same count of change values produced
+    rolling_delta_value_count = month_count - 1
+    #remove the first p/l value that was unintentionally included in the change values list so it doesn't inflate the average
+    rolling_change_list.pop(0)
+    #calculate the average change as an output of the sum of all change values mod count of change values; rounding to second decimal value.
+    average_change = round(sum(rolling_change_list)/(rolling_delta_value_count),2)
+    
+    #print print print!
 
-#Step 4: Pull out the respective min/max decrease and increase from the monthly_profit_delta list
-min_value = min(monthly_profit_delta)
-max_value = max(monthly_profit_delta)
+    print("Financial Analysis Challenge")
+    print( "---------------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Total Amount: ${total_pl}")
+    print(f"Average Change: ${average_change}")
+    print(f"Greatest Increase in Profits: {max_value_month} (${max_value})")
+    print(f"Greatest Decrease in Profits: {min_value_month} (${min_value})")
 
-#Step 5: Using index method() - assign the month variable with the min/max value of the delta list based on the relative min/max values 
-min_value_month = monthly_profit_delta.index(min(monthly_profit_delta))
-max_value_month = monthly_profit_delta.index(max(monthly_profit_delta))
+    #write write write!
 
-total_pl_output = str(sum(total_pl))
+    text_file_path="Analysis/Summary_Financial_Analysis.txt"
 
-#Step 6: Print Print Print the outputs!
-print("Financial Analysis")
-print("----------------------------------------------")
-print("Total Months :"+ month_count)
-print("Total: $"+ total_pl_output)
+    with open(text_file_path,"w") as text_file:
 
-#Step 7: Write Write Write the outputs!
+            text_file.write("Financial Analysis Challenge\n")
+            text_file.write( "---------------------------\n")
+            text_file.write(f"Total Months: {month_count}\n")
+            text_file.write(f"Total $: {total_pl}\n")
+            text_file.write(f"Average Change: ${average_change}\n")
+            text_file.write(f"Greatest Increase in Profits: {max_value_month} (${max_value})\n")
+            text_file.write(f"Greatest Increase in Profits: {min_value_month} (${min_value})\n")
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,18 @@
+#step 0: import the csv
 import csv
 
 #step 1: identify the csv path
 
 csvfilepath = "Resources/election_data.csv"
 
-vote_count = 0
+#set the variables
+total_vote_count = 0
+candidate_names = []
 candidate_dict = {}
+winner = ""
+winner_count = 0
 
-#Open the file and let's get started! 
+#step 3: open the file and let's get started! 
 with open(csvfilepath,mode='r',encoding='UTF-8') as csv_file:
     csv_data = csv.reader(csv_file,delimiter=',')
 
@@ -15,15 +20,37 @@ with open(csvfilepath,mode='r',encoding='UTF-8') as csv_file:
 #sum each key's total votes (votes per candidate/total votes variable) and print both the count and as a percentage
 #iterate through total votes counts and print the key associated with the highest count
 
-    #Step 0: Skip the headers for all iteration runs!
+    #Step 4: skip the headers for all iteration runs!
     headers = next(csv_data)
 
+#iterate through the records to produce the total amount of votes
     for i in csv_data:
-        vote_count = vote_count + 1
+        total_vote_count = total_vote_count + 1
         candidate = i[2]
-        candidate_vote_count = 0
-        if candidate not in candidate_dict:
-            candidate_dict[candidate],[candidate_vote_count] = i[2], candidate_vote_count +1
-
-    print(f"Total Votes {vote_count}")
-    print(f"Unique candidates from dictionary: {candidate_dict}")
+        #if the next record changes in candidate name, then procieed to add the candidate name to the candidate name list 
+        if candidate not in candidate_names:
+            candidate_names.append(candidate)
+            #Add the new candidate's name as a key to the candidate dictionary with a baseline vote count of 0
+            candidate_dict[candidate] = 0
+        #continue adding votes until the next candidate name is identified or the iteration runs out
+        candidate_dict[candidate] += 1
+     
+     #iterate through the newly created candidate dictionary and retrieve the value to evaulate which record contains the highest vote count produced from the previous iteration       
+    for i in candidate_dict:
+        candidate_votes_count = candidate_dict.get(i)
+    
+        if candidate_votes_count > winner_count:
+            winner_count = candidate_votes_count
+            #declare the winner of the most votes
+            winner = i       
+    
+    #print print print!
+    print("Election Results:")
+    print("-----------------------")
+    print(f"Total Votes {total_vote_count}")
+    print("-----------------------")
+    for i, k in candidate_dict.items():
+        print(f"{i} {k} {round(float(k/total_vote_count),2)*100}")
+    print("-----------------------")
+    print(f"Winner: {winner}")
+    print("-----------------------")
